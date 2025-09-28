9476af7602569e1852211b49a3e4a5881157fcc9
Andony Núñez <andony91@gmail.com>
2025-09-28 16:40:11 -0600

    Updating Dockerfile and Jenkinsfile

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,17 @@
 variable "docker_registry" {
   description = "Docker registry to push images to"
   type        = string
+  default     = "localhost:5000"
 }
 
 variable "api_image" {
-	description = "The docker image name for the backend API"
-	type        = string
-	default     = "store-api"
+  description = "The docker image name for the backend API"
+  type        = string
+  default     = "store-api"
 }
 
 variable "api_image_tag" {
-	description = "The docker image tag for the backend API"
-	type        = string
-	default     = "latest"
+  description = "The docker image tag for the backend API"
+  type        = string
+  default     = "latest"
 }
