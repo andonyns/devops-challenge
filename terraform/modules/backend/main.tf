800ee78923ddae5715ab711b45c2e15777320403
Andony Núñez <andony91@gmail.com>
2025-09-29 08:25:12 -0600

    Rename helm chart

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "backend" {
   depends_on = [var.dependency]
 
   provisioner "local-exec" {
-    command = "helm upgrade --install backend ${path.module}/../../../kubernetes/helm-chart --set image.repository=${var.docker_registry}/${var.api_image} --set image.tag=${var.api_image_tag} --kube-context minikube"
+    command = "helm upgrade --install backend ${path.module}/../../../kubernetes/application-helm-chart --set image.repository=${var.docker_registry}/${var.api_image} --set image.tag=${var.api_image_tag} --kube-context minikube"
   }
 }
 
